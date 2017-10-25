2411cd664a3bebba2387e14e2818e780403a82c4
“Veronica <vmatovich@gmail.com>
2017-10-25 15:55:11 +0000

    Permissions added to prevent unauthorized addition of sections to courses and unathorized access to instructor pages

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,8 +1,10 @@
 class Instructor::SectionsController < ApplicationController
 
-  def new
+  before_action :authenticate_user!
 
-    @course = Course.find(params[:course_id])
+  before_action :require_authorized_for_current_course
+
+  def new
 
     @section = Section.new
 
@@ -10,15 +12,31 @@ class Instructor::SectionsController < ApplicationController
 
   def create
 
-    @course = Course.find(params[:course_id])
+    @section = current_course.sections.create(sections_params)
 
-    @section = @course.sections.create(sections_params)
-
-    redirect_to instructor_course_path(@course)
+    redirect_to instructor_course_path(current_course)
 
   end
 
   private
+
+  def require_authorized_for_current_course
+
+    if current_course.user != current_user
+
+      render plain: "Unauthorized", status: :unauthorized
+
+    end
+
+  end
+
+  helper_method :current_course
+
+  def current_course
+
+    @current_course ||= Course.find(params[:course_id])
+
+  end
 
   def sections_params
 
